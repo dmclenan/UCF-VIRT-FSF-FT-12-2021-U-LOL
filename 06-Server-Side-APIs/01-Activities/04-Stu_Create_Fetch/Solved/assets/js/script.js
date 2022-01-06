1920ee4b7b9efcfcbd2869df351ff5914d10d1b9
Veronica Harris <veronica.n.harris92@gmail.com>
2022-01-06 13:50:49 -0500

    added notes to solved file of  activity 4 unit 6

--- 06-Server-Side-APIs/01-Activities/04-Stu_Create_Fetch/Solved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/04-Stu_Create_Fetch/Solved/assets/js/script.js
@@ -10,9 +10,15 @@ function getApi() {
       return response.json();
     })
     .then(function (data) {
+       // Check the data
+       console.log(repos)
+
+       //parsing the data
       for (var i = 0; i < data.length; i++) {
         var listItem = document.createElement('li');
+        //access
         listItem.textContent = data[i].html_url;
+        //update the page
         repoList.appendChild(listItem);
       }
     });
